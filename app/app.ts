be8418c016356cd28df5e16f7a60a432e9192281
Joao Victor <victorsrodrigues99@gmail.com>
2024-04-19 19:33:17 -0300

    Modelo de projeto, configuração da negociação

---
+++ app/app.ts
@@ -0,0 +1,10 @@
+import { NegociacaoContoller } from "./controllers/negociacao-controller.js";
+
+const controller = new NegociacaoContoller
+
+const form = document.querySelector('.form');
+
+form.addEventListener('submit', event => {
+    event.preventDefault();
+    controller.adiciona();
+})
---
+++ dist/js/app.js
@@ -0,0 +1,7 @@
+import { NegociacaoContoller } from "./controllers/negociacao-controller.js";
+const controller = new NegociacaoContoller;
+const form = document.querySelector('.form');
+form.addEventListener('submit', event => {
+    event.preventDefault();
+    controller.adiciona();
+});
