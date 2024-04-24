b0984b0cfd531f30738670b8d0b4fc02ed04a63a
Joao Victor <victorsrodrigues99@gmail.com>
2024-04-23 21:49:04 -0300

    Configurando modo de acesso a classes, herança

--- app/controllers/negociacao-controller.ts
+++ app/controllers/negociacao-controller.ts
@@ -2,7 +2,6 @@ import { Negociacao } from "../models/negociacao.js";
 import { Negociacoes } from "../models/negociacoes.js";
 import { MensagemView } from "../views/mensagem-view.js";
 import { NegociacoesView } from "../views/negociacoes-view.js";
-import { View } from "../views/view.js";
 
 export class NegociacaoController {
     private inputData: HTMLInputElement;
@@ -11,7 +10,7 @@ export class NegociacaoController {
     private negociacoes = new Negociacoes();
     private negociacoesView = new NegociacoesView('#negociacoesView');
     private mensagemView = new MensagemView('#mensagemView');
-    private view = new View('');
+    
 
     constructor(){
         this.inputData = document.querySelector('#data');
@@ -20,26 +19,37 @@ export class NegociacaoController {
         this.negociacoesView.update(this.negociacoes);
     }
 
-    adiciona() {  
+    public adiciona() {  
         const negociacao = this.criaNegociacao();
-        this.negociacoes.adiciona(negociacao); 
-        this.negociacoesView.update(this.negociacoes);
-        this.mensagemView.update('Negociação adicionada com sucesso');
-        this.limparFormulario();
+        if(negociacao.data.getDay()> 0 && negociacao.data.getDay() < 6){
+            this.negociacoes.adiciona(negociacao); 
+            this.limparFormulario();
+            this.atualizaView();
+             } else {
+                this.mensagemView.update('Apenas negociações em dias úteis são aceitas')
+             }
+        }
+
+        private criaNegociacao () {
+            const exp = /-/g;
+            const date = new Date(this.inputData.value.replace(exp, ','));
+            const quantidade = parseInt(this.inputQuantidade.value);
+            const valor = parseFloat(this.inputValor.value);
+            return new Negociacao(date, quantidade, valor);  
+        }
+    
+        private limparFormulario(): void {
+            this.inputData.value = '';
+            this.inputQuantidade.value = '';
+            this.inputValor.value = '';
+            this.inputData.focus();
+        }
+    
+        private atualizaView (): void{
+            this.negociacoesView.update(this.negociacoes);
+            this.mensagemView.update('Negociação adicionada com sucesso');
+        }
+   
     }
 
-    criaNegociacao () {
-        const exp = /-/g;
-        const date = new Date(this.inputData.value.replace(exp, ','));
-        const quantidade = parseInt(this.inputQuantidade.value);
-        const valor = parseFloat(this.inputValor.value);
-        return new Negociacao(date, quantidade, valor);  
-    }
-
-    limparFormulario(): void {
-        this.inputData.value = '';
-        this.inputQuantidade.value = '';
-        this.inputValor.value = '';
-        this.inputData.focus();
-    }
-}
+   
